981cf290054c5b07da5b22dae8b832b5c967fe06
Robson Moraes Cardoso <48536325+RobsonMora@users.noreply.github.com>
2019-11-15 14:22:34 -0300

    celulas editaveis e debug

--- scripts.js
+++ scripts.js
@@ -1,22 +1,4 @@
-/*
-function addColumn() {
 
-    var i = 0
-
-    document.getElementById('colunas').innerHTML = document.getElementById('colunas').innerHTML + '<th scope="col"  contenteditable="false">four</th>'
-    var linhas = document.getElementById('linhas').childElementCount;
-    for (i = 0; i < linhas; i++) {
-        var eLinhas = document.getElementById('linhas')
-        eLinhas.getElementById(i).innerHTML = eLinhas.getElementById(i).innerHTML + '<td></td>'
-    }
-}
-
-function addRow() {
-    var count = document.getElementById('linhas').childElementCount + 1
-
-    document.getElementById('linhas').innerHTML = document.getElementById('linhas').innerHTML + '<tr id="' + count + '">' + ' <th scope="row" contenteditable="false" >' + count + '</th>' + document.getElementById('celulas').innerHTML + '</tr>'
-}
-*/
 function addLine(idTable) {
     var table = document.getElementById(idTable);
     var rowNumber = table.rows.length;
@@ -27,6 +9,8 @@ function addLine(idTable) {
         cell = row.insertCell(0);
         if (i == collNumber - 1) {
             cell.innerHTML = rowNumber + ' ' + ' ';
+        }else{
+            cell.setAttribute('contenteditable', 'true')
         }
     }
 }
@@ -35,17 +19,21 @@ function addColl(idTable, inputId) {
     var input = document.getElementById(inputId)
     var tbl = document.getElementById(idTable),
         i;
+    var cell; 
+    var txt
+    cell = undefined
     if (input.value == '') {
         alert('Informe o valor da coluna!')
     } else {
-        for (i = 0; i < tbl.rows.length; i++) {
-            var txt;
-
+        for (i = 0; i < tbl.rows.length; i++) {           
+            txt = ''
             if (i == 0) {
                 txt = input.value;
                 input.value = '';
             }
-            addCells(tbl.rows[i].insertCell(tbl.rows[i].cells.length), txt, 'col');
+            cell = tbl.rows[i].insertCell(tbl.rows[i].cells.length)
+            cell.setAttribute('contenteditable', 'true')
+            addCells(cell, txt, 'col');
         }
     }
 }
